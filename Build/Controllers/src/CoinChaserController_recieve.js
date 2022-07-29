17e7b5161f988bc79aaf8ac33ef772cd7f9874d0
“AbubakrMahmood” <abubakrmahmood@hotmail.co.uk>
2022-07-29 13:55:26 +0100

    Fixing Joystick

--- Build/Controllers/src/CoinChaserController_recieve.js
+++ Build/Controllers/src/CoinChaserController_recieve.js
@@ -29,39 +29,45 @@ simplePeer.on("connection", function (data) {
   coordinates[data.peer] = { x: 700, y: 300 }; //data.peer is a peer id of the connected smartphone, used to identify a user and acces the correct coordinates
 });
 
+
+simplePeer.on("data", function(message){
+  console.log("This is message:");
+  console.log(message);
+});
+
 //run an update function to continuously process the data from the phone
-processData();
+// processData();
 
-//a function that loops over the connected joysticks and lets each player move a ball on the screen
-function processData() {
-  //clear canvas for a new frame
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  var i = 0;
-  //loop over the controller list
-  for (var key in simplePeer.controllerList) {
-    var joystick = simplePeer.controllerList[key];
-    console.log(joystick.messagesPerSecond);
-    //check if the joystick is being used
-    if (joystick.isActive) {
-      //console.log(joystick.ping);
-      //use the peer id to access the matching coordinate pair and add position change to the current player position
-      coordinates[joystick.peer.peer].x += joystick.positionChange.x;
-      coordinates[joystick.peer.peer].y += joystick.positionChange.y;
+// //a function that loops over the connected joysticks and lets each player move a ball on the screen
+// function processData() {
+//   //clear canvas for a new frame
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   var i = 0;
+//   //loop over the controller list
+//   for (var key in simplePeer.controllerList) {
+//     var joystick = simplePeer.controllerList[key];
+//     console.log(joystick.messagesPerSecond);
+//     //check if the joystick is being used
+//     if (joystick.isActive) {
+//       //console.log(joystick.ping);
+//       //use the peer id to access the matching coordinate pair and add position change to the current player position
+//       coordinates[joystick.peer.peer].x += joystick.positionChange.x;
+//       coordinates[joystick.peer.peer].y += joystick.positionChange.y;
 
-      //draw a ball for the new position
-      ctx.fillStyle = colours[i];
-      i += 1;
-      ctx.beginPath();
-      ctx.arc(
-        coordinates[joystick.peer.peer].x,
-        -coordinates[joystick.peer.peer].y,
-        10,
-        0,
-        2 * Math.PI
-      );
-      ctx.stroke();
-      ctx.fill();
-    }
-  }
-  requestAnimationFrame(processData);
-}
+//       //draw a ball for the new position
+//       ctx.fillStyle = colours[i];
+//       i += 1;
+//       ctx.beginPath();
+//       ctx.arc(
+//         coordinates[joystick.peer.peer].x,
+//         -coordinates[joystick.peer.peer].y,
+//         10,
+//         0,
+//         2 * Math.PI
+//       );
+//       ctx.stroke();
+//       ctx.fill();
+//     }
+//   }
+//   requestAnimationFrame(processData);
+// }
